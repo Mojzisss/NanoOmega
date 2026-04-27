45f3579ea59c6d12670c164e7ca97b7d58c26b13
tomanek <mojmirmojma@gmail.com>
2026-04-27 13:38:30 +0200

    button game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+
+        JFrame windowMain = new JFrame("mainWindow");
+        windowMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowMain.setSize(1500, 900);
+        windowMain.setLocationRelativeTo(null);
+        windowMain.setResizable(false);
+        windowMain.setVisible(true);
+
+
+        Random rd = new Random();
+        int heightRd = rd.nextInt(100,800);
+        int widthRd = rd.nextInt(100,1400);
+        JButton button = new JButton("button");
+        windowMain.add(button);
+        windowMain.revalidate();
+        button.setSize(100,100);
+        button.setLocation(0,100);
 
     }
 }
