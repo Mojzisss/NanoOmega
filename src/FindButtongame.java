11a20b72cd40862026c17b897031a1c994bea551
mojzisss <mojmirmojma@gmail.com>
2026-05-24 16:54:51 +0200

    game in own class

--- src/FindButtongame.java
+++ src/FindButtongame.java
@@ -1,6 +1,96 @@
 //import javax.swing.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 //import java.util.Random;
 //
-//public class FindButtongame extends JButton{
+//public class FindButtongame implements ActionListener {
 //
+//    private JButton button;
+//    private JFrame windowMain;
+//    private int windowwidth;
+//    private int windowheight;
+//    private Random rd;
+//    private int clicked = 0;
+//
+//    public FindButtongame(JButton button, JFrame windowMain,int windowvidth ,int windowheight, Random rd) {
+//        this.button = button;
+//        this.windowMain = windowMain;
+//        this.windowwidth = windowvidth;
+//        this.windowheight = windowheight;
+//        this.rd = rd;
+//    }
+//
+//    private int widthButton = windowwidth-100;
+//    private int heightButton = windowheight-100;
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        clicked++;
+//        double randomwidth = rd.nextDouble(0.3, 0.9);
+//        double randomheight = rd.nextDouble(0.3, 0.9);
+//
+//        widthButton =(int)(widthButton * randomwidth);
+//        heightButton =(int)(heightButton * randomheight);
+//
+//        int maxWidth = windowMain.getWidth() - button.getWidth();
+//        int maxHeight = windowMain.getHeight() - button.getHeight();
+//
+//        int x = rd.nextInt(maxWidth);
+//        int y = rd.nextInt(maxHeight);
+//
+//        button.setLocation(x, y);
+//        button.setSize(heightButton, widthButton);
+//
+//        System.out.println(clicked);
+//    }
 //}
+//
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class FindButtongame implements ActionListener {
+
+    private JButton button;
+    private JFrame windowMain;
+    private Random rd;
+
+    private int clicked = 0;
+
+    private int buttonSize = 100;
+
+    public FindButtongame(
+            JButton button,
+            JFrame windowMain,
+            Random rd
+    ) {
+        this.button = button;
+        this.windowMain = windowMain;
+        this.rd = rd;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        clicked++;
+
+        double random = rd.nextDouble(0.3, 0.9);
+
+        buttonSize = (int) (buttonSize * random);
+
+        int maxWidth =
+                windowMain.getWidth() - buttonSize;
+
+        int maxHeight =
+                windowMain.getHeight() - buttonSize;
+
+        int x = rd.nextInt(maxWidth);
+        int y = rd.nextInt(maxHeight);
+
+        button.setSize(buttonSize, buttonSize);
+        button.setLocation(x, y);
+
+        System.out.println(clicked);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,26 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
-        //Start start = new Start();
+
         JFrame windowMain = new JFrame("mainWindow");
         windowMain.setExtendedState(JFrame.MAXIMIZED_BOTH);
         windowMain.setLayout(null);
-        windowMain.setVisible(true);
         windowMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JButton button = new JButton("🦆");
+        button.setBounds(300, 300, 500, 500);
+
         Random rd = new Random();
-        JButton button = new JButton("button");
-        final int[] buttonsize = {windowMain.getHeight() - 100};
-        button.setSize(buttonsize[0], buttonsize[0]);
-        button.setLocation(200, 200);
+
         windowMain.add(button);
-        final int[] clicked = {0};
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                clicked[0]++;
-                double random = rd.nextDouble(0.3, 0.8);
-                buttonsize[0] = (int) (buttonsize[0] * random);
-                int MaxWidth = windowMain.getWidth() - button.getWidth();
-                int MaxHeight = windowMain.getHeight() - button.getHeight();
+        windowMain.setVisible(true);
 
-                int x = rd.nextInt(MaxWidth);
-                int y = rd.nextInt(MaxHeight);
-
-                button.setLocation(x, y);
-                button.setSize(buttonsize[0], buttonsize[0]);
-                System.out.println(clicked[0]);
-            }
-        });
-
+        button.addActionListener(
+                new FindButtongame(button, windowMain, rd)
+        );
     }
 }
