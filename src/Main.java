d67afa0938939382277e72ae2831772a3e1a9355
Mojzisss <mojmirmojma@gmail.com>
2026-05-31 23:53:42 +0200

    final ?

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,3 @@ public class Main {
         new Start();
     }
 }
-
-//private long startTime;
-//
-
-//startTime = System.currentTimeMillis();
-//
-//
-// private void checkWin() {
-//
-//    if (buttons.isEmpty()) {
-//
-//        long duration = System.currentTimeMillis() - startTime;
-//
-//        double seconds = duration / 1000.0;
-//
-//        JOptionPane.showMessageDialog(
-//                frame,
-//                "You Win! \nin: "
-//                        + String.format("%.2f", seconds) + " s"
-//        );
-//    }
-//}
